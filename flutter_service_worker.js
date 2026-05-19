c88e15666ccb89455250f70f2c5a2d86be87d257
Salman Khan <sal.khanpk@gmail.com>
2026-05-19 13:58:27 +0500

    Deploy Flutter web UI

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2aae0ccce98528700119d5dcc24fa498",
+const RESOURCES = {"flutter_bootstrap.js": "14df87f2ee5105a7308614fe864a9fbf",
 "version.json": "191595f72c94967ff44fc676a3aedb8c",
 "index.html": "456ca51b069e760255e5321f80c491a7",
 "/": "456ca51b069e760255e5321f80c491a7",
-"main.dart.js": "72a7b9e9a576d0be1aef240f209a6806",
+"main.dart.js": "bef56aa4bb99d69f02ae006145ab81bd",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "1521dbb3f2d122d185c46be692368828",
 "icons/Icon-192.png": "36d7863b2faf217493961a51ffd4f328",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "2aae0ccce98528700119d5dcc24fa498",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "c9d0fa7072eee4f03e2a1c1f6f2fa279",
+"assets/fonts/MaterialIcons-Regular.otf": "07c2b04c12594044dc9d304c0aae0c40",
 "canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
 "canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da4",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
