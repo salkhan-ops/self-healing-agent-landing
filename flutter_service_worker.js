51b3b7c7852509785841a2ab3505a2cc7dfbda6c
Salman Khan <sal.khanpk@gmail.com>
2026-06-04 12:19:37 +0500

    updated UI and backend

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "598378397891445f32b2bf330623a12c",
+const RESOURCES = {"flutter_bootstrap.js": "14c09866d548b946360f1c86b4384835",
 "version.json": "191595f72c94967ff44fc676a3aedb8c",
 "index.html": "610d620651ed06cbd81337ea2213c3e9",
 "/": "610d620651ed06cbd81337ea2213c3e9",
-"main.dart.js": "6e855b659b108b356c628f6e314e098c",
+"main.dart.js": "66a7b9dc155d5fe0a0708d26c639a5ce",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "1521dbb3f2d122d185c46be692368828",
 "icons/Icon-192.png": "36d7863b2faf217493961a51ffd4f328",
